7b4817aca77fd9e13be7bb93f7958a34fd55c38d
Gilles <gilolivier@users.noreply.github.com>
2025-03-08 09:48:47 +0000

    add autosuggest dropdown feature
    install autosuggest comp, refactor input slightly, add styles

--- app/components/input/index.tsx
+++ app/components/input/index.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { FC, useState, useEffect } from "react";
+import { FC, useState } from "react";
+import Autosuggest from "react-autosuggest";
 import { InputFieldInterface } from "./types";
 import { citySuggestionApiConfig } from "../../engine/api-config";
 
-const getCities = async (inputValue: string) => {
+const getCities = async ({
+  value,
+  setSuggestions,
+}: {
+  value: string;
+  setSuggestions: (arg: string[]) => void;
+}) => {
   const options = {
     ...citySuggestionApiConfig.requestOptions,
     body: JSON.stringify({
@@ -12,7 +19,7 @@ const getCities = async (inputValue: string) => {
         {
           parts: [
             {
-              // [TODO] this prompt needs some  serious refactoring and cleani
+              // [TODO] this prompt needs some  serious refactoring and cleaning up
               // 1. move to a config
               // 2. clean up  the result from the LLM
               // 3. clean up empty results
@@ -20,7 +27,7 @@ const getCities = async (inputValue: string) => {
               // 5. Move response formatting to use schema through the model config
               // 6. cater for errors
               // 7. sanitise user inputs
-              text: `Return a unique set of 20 cities names in the world that contains the following word: ${inputValue}. Do a simple string match on the string.
+              text: `Return a unique set of the 10 most relevant cities names in the world that contains the following word: ${value}. Do a simple string match comparaison.
                 Return the results in  JSON format: Return: Array<CityName>
                 Strip everything else from the response.
                 If there are no cities that match the prompt, return an empty array.`,
@@ -40,7 +47,7 @@ const getCities = async (inputValue: string) => {
       options,
     );
     const result = await response.json();
-    console.log(JSON.parse(result.candidates[0].content.parts[0].text));
+    setSuggestions(JSON.parse(result.candidates[0].content.parts[0].text));
   } catch (error) {
     // [TODO] cater for errors here
     console.error(error);
@@ -49,19 +56,30 @@ const getCities = async (inputValue: string) => {
 
 export const InputField: FC<InputFieldInterface> = ({ placeholder }) => {
   const [inputVal, setInputVal] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-  useEffect(() => {
-    if (inputVal !== "") {
-      getCities(inputVal);
-    }
-  }, [inputVal]);
+  const inputProps = {
+    className: "box-border border-1 w-150 block text-5xl cursor-auto p-2",
+    placeholder,
+    onChange: (e: React.SyntheticEvent) =>
+      setInputVal((e.target as HTMLInputElement).value),
+    value: inputVal,
+  };
 
   return (
-    <input
-      className="box-border border-1 w-150 block text-5xl cursor-auto p-2"
-      placeholder={placeholder}
-      onChange={(e) => setInputVal(e.target.value)}
-      value={inputVal}
+    <Autosuggest
+      suggestions={suggestions}
+      onSuggestionsFetchRequested={({ value }: { value: string }) => {
+        if (value !== "") {
+          getCities({ value, setSuggestions });
+        }
+      }}
+      onSuggestionsClearRequested={() => setSuggestions([])}
+      getSuggestionValue={(suggestion: { text: string }) => suggestion.text}
+      renderSuggestion={(suggestion: string) => {
+        return <span>{suggestion}</span>;
+      }}
+      inputProps={inputProps}
     />
   );
 };
---
+++ declarations.d.ts
@@ -0,0 +1,2 @@
+declare module "react-autosuggest";
+declare module "react-themeable";
