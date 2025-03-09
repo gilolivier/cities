04a88463e449dec8981c7abe9670249ed9ad856a
Gilles <gilolivier@users.noreply.github.com>
2025-03-09 20:28:15 +0000

    move gemini api key to env variable
    because, yeah, don't expose keys to a public repo....

--- app/engine/api-config.ts
+++ app/engine/api-config.ts
@@ -1,8 +1,7 @@
 import { CitySuggestionApiConfigInterface } from "./api-config-types";
 
 export const citySuggestionApiConfig: CitySuggestionApiConfigInterface = {
-  endpoint:
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBZIM7mTeaHwCkPEXwmHdzJKOv-ji2Bsfk",
+  endpoint: `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.NEXT_PUBLIC_GEMINI_API_KEY}`,
   requestOptions: {
     method: "POST",
     headers: { "Content-Type": "application/json" },
