bb606d838b84a8a9019017c0519ce6034f529204
Gilles <gilolivier@users.noreply.github.com>
2025-03-09 21:11:59 +0000

    tweak app title/label

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import { InputField } from "./components/input";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <InputLabel labelText="Venues near me" />
-      <InputField placeholder="start typing a city" />
+      <InputLabel labelText="Find music venues near me" />
+      <InputField placeholder="Start typing a city" />
     </div>
   );
 }
